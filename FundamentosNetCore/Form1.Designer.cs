f83aff8f5473ef4be1d09cbac5e1c6a91299387f
serraguti <pacoserranox@gmail.com>
2024-12-10 12:50:41 +0100

    Add project files.

---
+++ FundamentosNetCore/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnPulsar = new Button();
+            txtNombre = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnPulsar
+            // 
+            btnPulsar.Location = new Point(36, 132);
+            btnPulsar.Name = "btnPulsar";
+            btnPulsar.Size = new Size(179, 80);
+            btnPulsar.TabIndex = 0;
+            btnPulsar.Text = "Pulsar";
+            btnPulsar.UseVisualStyleBackColor = true;
+            btnPulsar.Click += btnPulsar_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(234, 44);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(221, 39);
+            txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(216, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(252, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Introduzca un nombre";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(571, 293);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(btnPulsar);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(6);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnPulsar;
+        private TextBox txtNombre;
+        private Label label1;
+    }
+}
---
+++ FundamentosNetCore/Form1.cs
@@ -0,0 +1,22 @@
+namespace FundamentosNetCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            //SOLO ESCRIBIMOS AQUI
+            this.txtNombre.Location = new Point(20, 10);
+            this.txtNombre.Text = "Soy un string";
+            this.txtNombre.Width = 220;
+            this.txtNombre.TextAlign = HorizontalAlignment.Right;
+            this.btnPulsar.BackColor =
+                Color.Fuchsia;
+        }
+    }
+}
